ed83a9a83d57a08fe2f5f8e92e9c162f8b52e93f
John Saber <jnsaber@emawellness.com>
2023-03-02 13:44:37 -0500

    Initial Commit

--- ema_senseye/ema_senseyeApp.swift
+++ ema_senseye/ema_senseyeApp.swift
@@ -6,12 +6,29 @@
 //
 
 import SwiftUI
-
+import senseye_ios_sdk
 @main
 struct ema_senseyeApp: App {
+    @Environment(\.scenePhase) var scenePhase
+        var senseyeSDK: SenseyeSDK = SenseyeSDK(userId: "ema", taskIds: [.firstCalibration], databaseLocation: "ema_wellness")
+        let initialBrightness = UIScreen.main.brightness
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+           WindowGroup {
+               EntryView(senseyeSDK: senseyeSDK)
+           }
+           .onChange(of: scenePhase) { newScene in
+               if newScene == .active {
+                   DispatchQueue.main.async {
+                       UIScreen.main.brightness = 1.0
+                       UIApplication.shared.isIdleTimerDisabled = true
+                   }
+               } else {
+                   DispatchQueue.main.async {
+                       UIScreen.main.brightness = initialBrightness
+                       UIApplication.shared.isIdleTimerDisabled = false
+                   }
+               }
+           }
     }
 }
