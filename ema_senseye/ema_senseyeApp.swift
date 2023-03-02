f7772711493cc0f318540eba3c1aa83e1bca20a1
John Saber <jnsaber@emawellness.com>
2023-03-02 13:32:09 -0500

    Initial Commit

---
+++ ema_senseye/ema_senseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ema_senseyeApp.swift
+//  ema_senseye
+//
+//  Created by John Saber on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct ema_senseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
