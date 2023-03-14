9e9b422cf113c91231fa281606437e1d42e39edd
johnsabergithub <jnsaber@emawellness.com>
2023-03-14 12:51:47 -0400

    Read parameters from command line

--- ema_senseye/EntryView.swift
+++ ema_senseye/EntryView.swift
@@ -10,17 +10,22 @@ import Foundation
 import SwiftUI
 import senseye_ios_sdk
 
-
 struct EntryView: View {
-    @Binding var cameFromUrl:URL
-    
+    //@Evironment(\.deepLink) var deepLink: String
+     @Binding var cameFromUrl:URL
+    @Binding var urlLoaded:Bool
     //init() {
         //self.senseyeSDK = senseyeSDK
     //   }
 
-       var body: some View {
-        
-           var senseyeSDK = SenseyeSDK(userId: cameFromUrl.path(), taskIds: [.firstCalibration], databaseLocation: "ema_wellness")
-              senseyeSDK.senseyeTabView()
-                  }
+    var body: some View {
+        if self.urlLoaded
+        {
+            var link = cameFromUrl.absoluteString
+            let PatientId = link.replacingOccurrences(of: "emasenseye://", with: "")
+            var senseyeSDK = SenseyeSDK(userId: PatientId, taskIds: [.firstCalibration], databaseLocation: "ema_wellness")
+            senseyeSDK.senseyeTabView()
+            //Text("Came from \(link2)")
+        }
+    }
 }
--- ema_senseye/ema_senseyeApp.swift
+++ ema_senseye/ema_senseyeApp.swift
@@ -12,24 +12,37 @@ struct ema_senseyeApp: App {
     @Environment(\.scenePhase) var scenePhase
     //var senseyeSDK: SenseyeSDK = SenseyeSDK(userId: "john2", taskIds: [.firstCalibration], databaseLocation: "ema_wellness")
     //var senseyeSDK = SenseyeSDK()
-    @State var launchURL:URL = URL(fileURLWithPath: "https://exampleplacehoder.com")
+    @State var launchURL:URL = URL(fileURLWithPath: "https://www.example.com")
+    @State var URLLoaded:Bool = false
     let initialBrightness = UIScreen.main.brightness
-
+    
     var body: some Scene {
-           WindowGroup {
-               //EntryView(senseyeSDK: senseyeSDK).onOpenURL(perform: {url in print(url.absoluteString)})}.onChange(of: scenePhase) { newScene in
-               EntryView(cameFromUrl: $launchURL).onOpenURL(perform: {url in launchURL = url})}.onChange(of: scenePhase) { newScene in
-               if newScene == .active {
-                   DispatchQueue.main.async {
-                       UIScreen.main.brightness = 1.0
-                       UIApplication.shared.isIdleTimerDisabled = true
-                   }
-               } else {
-                   DispatchQueue.main.async {
-                       UIScreen.main.brightness = initialBrightness
-                       UIApplication.shared.isIdleTimerDisabled = false
-                   }
-               }
-           }
+        WindowGroup {
+            //.onOpenURL(perform: {url in print(url.absoluteString)})}.onChange(of: scenePhase) { newScene in
+            //EntryView(cameFromUrl: $launchURL).onOpenURL(perform: {url in launchURL = url})}.onChange(of: scenePhase)
+            EntryView(cameFromUrl: $launchURL, urlLoaded: $URLLoaded).onOpenURL(perform: handleURL).onChange(of: scenePhase)
+            { newScene in
+                if newScene == .active {
+                    DispatchQueue.main.async {
+                        UIScreen.main.brightness = 1.0
+                        UIApplication.shared.isIdleTimerDisabled = true
+                    }
+                } else {
+                    DispatchQueue.main.async {
+                        UIScreen.main.brightness = initialBrightness
+                        UIApplication.shared.isIdleTimerDisabled = false
+                    }
+                }
+            }
+        }}
+    
+    func handleURL(_ url: URL)
+    {
+        launchURL = url
+        URLLoaded = true
+        //print(url)
+        
+        //var senseyeSDK = SenseyeSDK(userId: url.lastPathComponent, taskIds: [.firstCalibration], databaseLocation: "ema_wellness")
+        //await senseyeSDK.senseyeTabView()
     }
 }
